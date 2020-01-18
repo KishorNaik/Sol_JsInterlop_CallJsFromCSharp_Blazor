c40f03dc420fa56b163a9857693ea9997a250cc8
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-18 10:01:34 +0530

    code commit

---
+++ Sol_Demo/Sol_Demo/wwwroot/demo.js
@@ -0,0 +1,3 @@
+﻿function showAlert() {
+    alert("Hello from JS");
+}
---
+++ Sol_Demo/Sol_Demo/wwwroot/js/demo.js
@@ -0,0 +1,15 @@
+﻿function showAlertFromJs() {
+    alert("Hello from JS");
+}
+
+function getValues(values) {
+    return values;
+}
+
+function setFocus(id) {
+    document.getElementById(id).focus();
+}
+
+function elementToFocus(element) {
+    element.focus();
+}
